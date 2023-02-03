ca25ecb15434cc49d8150ad726144773276a3f57
Alex Katz <alexanderikatz@gmail.com>
2023-02-03 16:09:16 -0500

    keymap reorg

--- init.lua
+++ init.lua
@@ -3,7 +3,6 @@ require('config.lazy')
 
 vim.cmd([[colorscheme arctic]])
 
--- diffview
 local open_diff_view = function()
   vim.ui.input({ prompt = 'Diff working tree with branch (leave blank for development): ' }, function(branch_name)
     require('diffview').open(string.len(branch_name) == 0 and 'development' or branch_name)
@@ -12,19 +11,26 @@ end
 
 require('which-key').register({
   ['<leader>'] = {
+    -- diffview
     g = {
       name = '+Git Diffview',
       g = { '<Cmd>DiffviewOpen<CR>', 'Diff Workspace changes' },
       b = { open_diff_view, 'Diff Workspace with branch (Development if left blank)' },
       d = { '<Cmd>DiffviewClose<CR>', 'Close Diffview' },
-      f = { '<Cmd>DiffviewFileHistory %<CR>', 'File history' },
+      f = { '<Cmd>DiffviewFileHistory %<CR>', 'File History (this file)' },
+      F = { '<Cmd>DiffviewFileHistory<CR>', 'File History' },
+    },
+
+    -- telescope undo
+    s = {
+      u = { '<Cmd>Telescope undo<CR>', 'Search Undo History' },
+    },
+
+    -- undo tree
+    U = { '<Cmd>UndotreeToggle<CR>', 'Toggle Undo Tree' },
+    w = {
+      v = { '<C-W>v', 'Vertical Split' },
+      h = { '<C-W>s', 'Horizontal Split' },
     },
   },
 })
-
-vim.keymap.set(
-  'n',
-  '<leader>su',
-  '<cmd>Telescope undo<cr>',
-  { noremap = true, silent = true, desc = 'Search Undo History' }
-)
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,19 +14,18 @@ vim.keymap.set('n', 'N', 'Nzzzv', { noremap = true, silent = true })
 vim.keymap.set({ 'i', 'v', 'n', 's' }, '<M-s>', '<cmd>w<cr><esc>', { desc = 'Save file' })
 vim.keymap.set({ 'i', 'v', 'n', 's' }, '<M-a>', '<cmd>wa<cr><esc>', { desc = 'Save file' })
 
--- vertical split
-vim.keymap.set('n', '<leader>wv', '<C-w>v', { desc = 'Vertical Split' })
+-- Resize window using <ctrl> arrow keys
+vim.keymap.set('n', '<M-Up>', '<cmd>resize +2<cr>', { desc = 'Increase window height' })
+vim.keymap.set('n', '<M-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease window height' })
+vim.keymap.set('n', '<M-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease window width' })
+vim.keymap.set('n', '<M-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase window width' })
 
--- remove lazygit bindings
+-- remove some git bindings
 vim.api.nvim_del_keymap('n', '<leader>gg')
 vim.api.nvim_del_keymap('n', '<leader>gG')
 vim.api.nvim_del_keymap('n', '<leader>gc')
 vim.api.nvim_del_keymap('n', '<leader>gs')
 
--- undo tree
-vim.keymap.set(
-  'n',
-  '<leader>U',
-  '<Cmd>UndotreeToggle<CR>',
-  { noremap = true, silent = true, desc = 'Toggle Undo Tree' }
-)
+-- remove some window bindings
+vim.api.nvim_del_keymap('n', '<leader>w|')
+vim.api.nvim_del_keymap('n', '<leader>w-')
