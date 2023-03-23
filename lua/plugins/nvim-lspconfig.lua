ceeea0d2d7a7d33fa35770dfe789ea1e762dc1c7
Alex Katz <alexanderikatz@gmail.com>
2023-03-23 18:07:30 -0400

    fixed eslint autoformatting issue

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -30,18 +30,6 @@ return {
           },
         },
       },
-
-      setup = {
-        eslint = function()
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            callback = function(event)
-              if require('lspconfig.util').get_active_client_by_name(event.buf, 'eslint') then
-                vim.cmd('EslintFixAll')
-              end
-            end,
-          })
-        end,
-      },
     },
   },
 }
