83fa8af5c799ed4e3f9049b7408cd171c1fc8b63
Alex Katz <alexanderikatz@gmail.com>
2023-04-11 14:59:05 -0400

    removed redundant colorscheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,14 +1,3 @@
 return {
-  {
-    'folke/tokyonight.nvim',
-    config = function()
-      require('tokyonight').setup({
-        style = 'night',
-        styles = {
-          functions = {},
-        },
-      })
-    end,
-  },
   { dir = '~/Developer/arctic.nvim', dependencies = { 'rktjmp/lush.nvim' } },
 }
