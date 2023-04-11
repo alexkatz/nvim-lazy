1ae93504e9e7ccd910ff2de9d947c935ffd32948
Alex Katz <alexanderikatz@gmail.com>
2023-04-11 15:40:52 -0400

    added catppuccin

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require('config.lazy')
 
-vim.cmd([[colorscheme arctic]])
+-- vim.cmd([[colorscheme arctic]])
+-- vim.cmd([[colorscheme catpuccin-macchiato]])
--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,3 +1,8 @@
 return {
+  {
+    'catppuccin/nvim',
+    lazy = true,
+    name = 'catppuccin',
+  },
   { dir = '~/Developer/arctic.nvim', dependencies = { 'rktjmp/lush.nvim' } },
 }
