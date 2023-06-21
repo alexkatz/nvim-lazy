5ffa7c0d726712b310ba1545b91b4d700d0f3e80
Alex Katz <alexanderikatz@gmail.com>
2023-06-21 11:31:22 -0400

    added flash

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,6 +25,7 @@ require('lazy').setup({
     { import = 'lazyvim.plugins.extras.ui.edgy' },
     { import = 'lazyvim.plugins.extras.vscode' },
     { import = 'lazyvim.plugins.extras.util.mini-hipatterns' },
+    { import = 'lazyvim.plugins.extras.editor.flash' },
     { import = 'plugins' },
   },
   defaults = {
