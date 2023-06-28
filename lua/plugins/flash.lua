9a5740c273fea02a8fdde49cd542473d315f618d
Alex Katz <alexanderikatz@gmail.com>
2023-06-28 11:31:32 -0400

    flash config updates, added yanky

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,11 +1,9 @@
 return {
   'folke/flash.nvim',
   opts = {
-    highlight = {
-      label = {
-        rainbow = {
-          enabled = true,
-        },
+    label = {
+      rainbow = {
+        enabled = true,
       },
     },
   },
