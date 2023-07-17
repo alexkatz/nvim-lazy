338cd4f3d31e1be9d5a8edae53dfe3147e363064
Alex Katz <alexanderikatz@gmail.com>
2023-07-17 11:14:54 -0400

    neotree 3 config updates

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -24,38 +24,6 @@ return {
       },
     },
     opts = {
-      default_component_configs = {
-        icon = {
-          folder_empty = '󰜌',
-          folder_empty_open = '󰜌',
-        },
-        git_status = {
-          symbols = {
-            renamed = '󰁕',
-            unstaged = '󰄱',
-          },
-        },
-      },
-      document_symbols = {
-        kinds = {
-          File = { icon = '󰈙', hl = 'Tag' },
-          Namespace = { icon = '󰌗', hl = 'Include' },
-          Package = { icon = '󰏖', hl = 'Label' },
-          Class = { icon = '󰌗', hl = 'Include' },
-          Property = { icon = '󰆧', hl = '@property' },
-          Enum = { icon = '󰒻', hl = '@number' },
-          Function = { icon = '󰊕', hl = 'Function' },
-          String = { icon = '󰀬', hl = 'String' },
-          Number = { icon = '󰎠', hl = 'Number' },
-          Array = { icon = '󰅪', hl = 'Type' },
-          Object = { icon = '󰅩', hl = 'Type' },
-          Key = { icon = '󰌋', hl = '' },
-          Struct = { icon = '󰌗', hl = 'Type' },
-          Operator = { icon = '󰆕', hl = 'Operator' },
-          TypeParameter = { icon = '󰊄', hl = 'Type' },
-          StaticMethod = { icon = '󰠄 ', hl = 'Function' },
-        },
-      },
       -- Add this section only if you've configured source selector.
       source_selector = {
         sources = {
@@ -83,7 +51,9 @@ return {
         },
       },
       filesystem = {
-        follow_current_file = true,
+        follow_current_file = {
+          enabled = true,
+        },
       },
     },
 
