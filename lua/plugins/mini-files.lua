88c784ee1d8a00a95080d34ef252f7907acabe85
Alex Katz <alexanderikatz@gmail.com>
2024-06-07 11:20:31 -0400

    fixed mini files split keybindings

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -13,24 +13,30 @@ return {
       width_focus = 40,
       width_preview = 100,
     }
+  end,
+  config = function(_, opts)
+    local MiniFiles = require('mini.files')
+    MiniFiles.setup(opts)
 
     local map_split = function(buf_id, lhs, direction)
-      local MiniFiles = require('mini.files')
-
       local rhs = function()
-        -- Make new window and set it as target
-        local new_target_window
-        vim.api.nvim_win_call(MiniFiles.get_target_window(), function()
+        -- Make new window, set it as target, close
+        local current_target_window = MiniFiles.get_target_window()
+        if not current_target_window then
+          return
+        end
+
+        vim.api.nvim_win_call(current_target_window, function()
           vim.cmd(direction .. ' split')
-          new_target_window = vim.api.nvim_get_current_win()
+          MiniFiles.set_target_window(vim.api.nvim_get_current_win())
         end)
 
-        MiniFiles.set_target_window(new_target_window)
-        MiniFiles.go_in({ close_on_file = true })
+        MiniFiles.close()
       end
 
       -- Adding `desc` will result into `show_help` entries
       local desc = 'Split ' .. direction
+
       vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
     end
 
@@ -38,9 +44,8 @@ return {
       pattern = 'MiniFilesBufferCreate',
       callback = function(args)
         local buf_id = args.data.buf_id
-        -- Tweak keys to your liking
-        map_split(buf_id, 'H', 'belowright horizontal')
-        map_split(buf_id, 'V', 'belowright vertical')
+        map_split(buf_id, 'gh', 'belowright horizontal')
+        map_split(buf_id, 'gv', 'belowright vertical')
       end,
     })
   end,
